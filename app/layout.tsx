57cedd6a65d726aa909202b83d297ac5eee7c78b
Daniel Gwak <gwakdaniel@gmail.com>
2024-11-05 18:20:29 -0500

    Connected web app to mysql database. Needed to change inbound rules for RDS security

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import { Inter } from 'next/font/google'
 import { ModalProvider } from '@/providers/modal-provider'
 
 import "./globals.css";
+import prismadb from "@/lib/prismadb";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -28,6 +29,7 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const store = prismadb.store
   return (
     <ClerkProvider>
       <html lang="en">
